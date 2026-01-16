8c94b29ea1164ba11e1e7d523a88f3b504d32598
Clemente T. Sánchez <sanchezb@mit.edu>
2026-01-16 19:48:49 -0300

    project reorganization

--- test/test_script.R
+++ test/test_script.R
@@ -1,12 +1,22 @@
 
-# This is a script intended to test the indenter
+# This is a script intended to test the IndenteR extension
 
 library(tidyverse)
 
 a <- 1:10
 b <- 1:10
 
+# Actual functionality ---------
+
 data <- tibble(a, b) |>
   # create new variables
 mutate(c = a + b,
        d = c * 10)
+
+
+# Intended functionality ----------
+
+data <- tibble(a, b) |>
+  # create new variables
+  mutate(c = a + b,
+         d = c * 10)
