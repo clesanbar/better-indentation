95692fc1261cbfa3e18ade1d0cc63545ad9ce8d8
Clemente T. Sánchez <sanchezb@mit.edu>
2026-01-23 08:37:31 -0300

    Update README and test script for indentation logic
    Added a new item to the README checklist regarding indentation reset after pipe chains. Updated test_script.R to clarify intended vs. current indentation behavior and expanded test cases for pipe chains.

--- README.md
+++ README.md
@@ -39,3 +39,4 @@ The first is what you get when you're writing in Positron, while the second is w
 
 - [x] Pipe indentation erroneously inherits deep indentation from aligned arguments.
 - [x] The indentation seems to reset when including a comment between a pipe and a function.
+- [] Reset indentation after the chain of pipes is over and the user wants to create a new object.
--- test/test_script.R
+++ test/test_script.R
@@ -1,22 +1,38 @@
 
-# This is a script intended to test the IndenteR extension
+# This is a script intended to test the indentR extension
 
 library(tidyverse)
 
 a <- 1:10
 b <- 1:10
 
-# Actual functionality ---------
+# Current implementation ---------
 
-data <- tibble(a, b) |>
+data <- tibble(a,b) |>
   # create new variables
-mutate(c = a + b,
-       d = c * 10)
+  mutate(c = c + b,
+         d = c * 10)
+
+         second_data <- data |>
+           # create new variables
+           mutate(f = c * d) |>
+             # subset to certain cases
+             filter(a == 4) |>
+               # and then keep a couple of variables
+               select(a, b, c)
 
 
 # Intended functionality ----------
 
-data <- tibble(a, b) |>
+data <- tibble(a,b) |>
   # create new variables
-  mutate(c = a + b,
+  mutate(c = c + b,
          d = c * 10)
+
+second_data <- data |>
+  # create new variables
+  mutate(f = c * d) |>
+  # subset to certain cases
+  filter(a == 4) |>
+  # and then keep a couple of variables
+  select(a, b, c)
